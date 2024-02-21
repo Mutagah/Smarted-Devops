efc13572052b6e8d48af72fb6598941e3956a6be
Mutagah <kihahumutagah@gmail.com>
2024-02-21 23:02:35 +0300

    python basics practice.

---
+++ practice/learn_python/receive_input.py
@@ -0,0 +1,38 @@
+# # input is used to read values from the terminal window
+# name = input("What is you name? ")
+# # String concatenation in python
+# print("Greetings "+ name)
+
+# # conversion of the value of a variable from one type to another.
+# # NB: Input values are always returned as a string
+# # int() converts the input into a type of integer.
+# # float() converts argurmeny provided to a float data type(This has decimals)
+# # bool() converts values to a boolean
+# # str() converts values to a string
+# birth_year = int(input("Enter your birth year: "))
+# print(2024 - birth_year) 
+
+# # Addition of numbers
+# first_number = int(input("Enter the first number: "))
+# second_number = int(input("Enter the second number: "))
+# print("Sum is "+ str(first_number + second_number))
+
+
+# >>>>>Strings<<<<
+# Strings are immutable, we cannot change them once we create them
+course = "python for beginners"
+print(course.find("for"))
+print(course.replace("for", "4"))
+print(course.capitalize())
+print(course.upper())
+print(course.lower())
+
+#>>>>arithmetic operators >> addition, subtraction, multiplication, division
+# Division with one slash and with division with two slashes
+print(10 / 3) #>>Returns a floating point number
+print(10 // 3) #>>Returns a integer
+# Modulus operator returns the remainder whereas double astericks means exponential
+
+# argumented assignment operator >> An example would be x += 3 
+
+# --------- operator precedence --------------
